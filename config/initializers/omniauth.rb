a4c53fd233be5092fb5ddf55428b3e1c43bd15f4
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-08-10 22:47:24 +0900

    twitter omniauth setting up.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, Rails.application.credentials[:twitter][:api_key],
+           Rails.application.credentials[:twitter][:api_secret]
+end
