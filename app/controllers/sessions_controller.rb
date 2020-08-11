40723f33fc23a3eab3e7131a06dea5c8b46c6988
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-08-12 01:00:18 +0900

    :+1: twitterでのログイン、ログアウト周りの処理の大枠を作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,36 @@
 class SessionsController < ApplicationController
+  def create
+    user_data = request.env['omniauth.auth']
+    unless user_data
+      flash[:alert] = 'twitterでのログインに失敗しました。'
+      redirect_to root_url
+    end
+
+    user = User.find_by(uid: user_data[:uid])
+    if user
+      flash[:success] = "#{user.nickname}様ようこそ！"
+      log_in(user) && redirect_to(root_url)
+      return
+    end
+
+    new_user = User.new(
+      uid: user_data[:uid],
+      nickname: user_data[:info][:nickname]
+    )
+    if new_user.save
+      log_in new_user
+      flash[:success] = 'ユーザ登録しました'
+    else
+      flash[:alert] = 'ユーザ登録に失敗しました'
+    end
+    redirect_to root_url
+  end
+
+  def destroy
+    if signed_in?
+      log_out
+      flash[:success] = 'ログアウトしました'
+    end
+    redirect_to root_url
+  end
 end
