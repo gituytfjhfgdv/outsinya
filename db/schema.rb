f1535bbcf69e64e1025639b23058f73694aa1f86
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-08-12 00:10:21 +0900

    :cat: rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_135950) do
+ActiveRecord::Schema.define(version: 2020_08_11_150554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_135950) do
     t.string "icon_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
   end
 
 end
