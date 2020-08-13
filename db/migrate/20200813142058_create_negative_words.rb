ca816c472fc43dc9c8ac9c8315680134f950eac6
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-08-13 23:22:14 +0900

    :nail_care: add null false to negative_word.content.

--- db/migrate/20200813142058_create_negative_words.rb
+++ db/migrate/20200813142058_create_negative_words.rb
@@ -1,7 +1,7 @@
 class CreateNegativeWords < ActiveRecord::Migration[6.0]
   def change
     create_table :negative_words, id: :uuid do |t|
-      t.string :content
+      t.string :content, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
