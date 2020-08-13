ee43a4fa6b4e54e046f5d95347e009dcbcc783ec
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-08-13 23:23:54 +0900

    :nail_care: set type uuid to foreign_key.

--- db/migrate/20200813142058_create_negative_words.rb
+++ db/migrate/20200813142058_create_negative_words.rb
@@ -2,7 +2,7 @@ class CreateNegativeWords < ActiveRecord::Migration[6.0]
   def change
     create_table :negative_words, id: :uuid do |t|
       t.string :content, null: false
-      t.references :user, null: false, foreign_key: true
+      t.references :user, type: :uuid, foreign_key: true
 
       t.timestamps
     end
