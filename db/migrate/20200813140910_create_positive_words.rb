c319d87ae6bceb70d7a6521170269e4b47d79d6e
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-08-13 23:11:26 +0900

    :nail_care: set positive_word.content null: false.

--- db/migrate/20200813140910_create_positive_words.rb
+++ db/migrate/20200813140910_create_positive_words.rb
@@ -1,7 +1,7 @@
 class CreatePositiveWords < ActiveRecord::Migration[6.0]
   def change
     create_table :positive_words, id: :uuid do |t|
-      t.string :content
+      t.string :content, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
