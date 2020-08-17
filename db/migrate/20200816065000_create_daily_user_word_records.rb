0686b71c7bfbb93ae2160babe0b3a31c7c40f38d
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-08-17 21:59:25 +0900

    :nail_care: set type uuid as foreign key

--- db/migrate/20200816065000_create_daily_user_word_records.rb
+++ db/migrate/20200816065000_create_daily_user_word_records.rb
@@ -1,9 +1,9 @@
 class CreateDailyUserWordRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :daily_user_word_records, id: :uuid do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :personal_word, null: false, foreign_key: true
-      t.integer :count
+      t.references :user, null: false, type: :uuid, foreign_key: true
+      t.references :personal_word, null: false, type: :uuid, foreign_key: true
+      t.integer :count, null: false
       t.datetime :created_on
 
       t.timestamps
