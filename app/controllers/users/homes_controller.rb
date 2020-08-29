5057507a2754759a7c893b06a51e151e00707650
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-08-29 21:07:24 +0900

    ユーザ画面のデザインを整える

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -1,7 +1,7 @@
 class Users::HomesController < UsersController
   def index
     @current_user = current_user
-    @yesterday_word_records = @current_user.daily_user_word_records.where(created_on: 1.day.ago.all_day)
+    @yesterday_word_records = @current_user.daily_user_word_records.includes(:personal_word).where(created_on: 1.day.ago.all_day)
     @positive_words = @current_user.positive_words.limit(5)
     @negative_words = @current_user.negative_words.limit(5)
   end
