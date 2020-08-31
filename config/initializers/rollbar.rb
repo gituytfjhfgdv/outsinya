3f3dc638fe2213c5b5fd0ec28583f28c35b136e1
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-09-01 01:02:52 +0900

    本番環境でのみrollbarの設定が走るようにする

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -68,4 +68,4 @@ Rollbar.configure do |config|
   # setup for Heroku. See:
   # https://devcenter.heroku.com/articles/deploying-to-a-custom-rails-environment
   config.environment = ENV['ROLLBAR_ENV'].presence || Rails.env
-end
+end if Rails.env.production?
